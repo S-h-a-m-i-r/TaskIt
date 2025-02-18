85b9628856e5350f9c515a026e3c7ea84a477622
S-h-a-m-i-r <shahshamu164@gmail.com>
2025-02-19 02:46:56 +0500

    Improve design

--- src/admin/index.tsx
+++ src/admin/index.tsx
@@ -1,10 +1,24 @@
+import { useNavigate, To } from "react-router-dom"
 import AdminTaskList from "./AdminTaskList"
+import notification from '../assets/icons/Notification_icon.svg';
+
 
 const AdminDashboard = () => {
+  const navigate = useNavigate()
+
+  const redirection = (path: To) => {
+    navigate(path)
+  }
   return (
     <div className='py-18 px-7 w-full'>
-      <div className='flex items-center gap-2'> 
+      <div className='flex justify-between gap-2'> 
         <span className='font-semibold text-2xl'> Dashboard Admin </span>
+        <button
+          className="bg-[#F9FAFB] border border-[#F9FAFB] rounded-full p-3 transition-all duration-1000"
+        onClick={()=>redirection('/notification')}
+        >
+          <img src={notification} />
+        </button>
       </div>
       <div className='w-full rounded-xl mt-8'>
         <AdminTaskList/>
