79e0658d6cb1600125ac5a322f5976cd82d4abfa
umarmaqbool <umarmaqbool370@gmail.com>
2025-09-28 16:30:50 +0500

    fix issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [],
-    },
-  },
-  optimizeDeps: {
-    include: ['@rollup/rollup-linux-x64-gnu']
-  }
 })
